ecee1ff453f727ca76fa8d74c60f226747d11cbe
NuriaRizzo <nrizzo2550@utm.edu.ec>
2026-05-13 10:42:03 -0500

    Actividad"2

---
+++ Calculadora_Web/calculadora_web/script.js
@@ -0,0 +1,37 @@
+// Selecciona la pantalla
+const pantalla = document.getElementById("pantalla");
+
+
+// 
+function agregar(valor){
+
+    pantalla.value += valor;
+}
+
+
+// 
+function limpiar(){
+
+    pantalla.value = "";
+}
+
+
+// 
+function borrar(){
+
+    pantalla.value = pantalla.value.slice(0, -1);
+}
+
+
+// 
+function calcular(){
+
+    try{
+
+        pantalla.value = eval(pantalla.value);
+
+    }catch(error){
+
+        pantalla.value = "Error";
+    }
+}
